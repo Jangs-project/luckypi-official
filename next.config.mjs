d0eb013390da8a92174d42ea47f9d6b785410dfd
Jangs-project <n0988576990@gmail.com>
2026-05-10 00:25:34 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
-```javascript
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  webpack: (config, { isServer }) => {
-    return config
-  },
 }
 
 export default nextConfig
-```

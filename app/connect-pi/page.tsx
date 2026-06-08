ef032dbc02d8f73b86b4517f1a2ad97a091cad36
Jangs-project <n0988576990@gmail.com>
2026-06-08 16:10:18 +0800

    Update page.tsx

--- app/connect-pi/page.tsx
+++ app/connect-pi/page.tsx
@@ -1,2 +1,5 @@
-Connect Your Pi Wallet Page
-Here, users can link their Pi wallet to the platform. They just enter their wallet address and hit connect. After that, they'll see their wallet address and balance displayed. From there, they can jump straight into the game lobby or head to the currency exchange. They can also disconnect their wallet whenever they want.
+First, you'll see a screen where you can type in your Pi wallet address and hit the "Connect Wallet" button to link your account.
+Once connected, you'll see your wallet address and your Pi balance displayed clearly.
+From there, you have two fun options: "Start Game" to jump into the lobby and play, or "Exchange" to swap your Pi for in-game coins.
+If you ever want to disconnect your wallet, just click the red "Disconnect" button and everything will be safely removed.
+There's also a handy "Back to Home" button at the top if you need to go to the main page.

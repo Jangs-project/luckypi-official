a6598f54351aca427757ed516ba3d949beb281e7
Jangs-project <n0988576990@gmail.com>
2026-06-08 14:44:18 +0800

    Create i18n.ts

---
+++ lib/i18n.ts
@@ -0,0 +1,2 @@
+Multilingual Support
+This part makes the platform welcoming to users from all over the world. It includes translations for 10 different languages, like English, Traditional Chinese, Japanese, Korean, Vietnamese, and more. A handy function automatically shows the correct language based on user preferences, so everyone feels at home.

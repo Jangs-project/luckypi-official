b2bd78286fa1c7ae146eb6e94ea2954a89d2fa5c
Jangs-project <n0988576990@gmail.com>
2026-06-08 14:42:36 +0800

    Create page.tsx

---
+++ app/connect-pi/page.tsx
@@ -0,0 +1,2 @@
+Connect Your Pi Wallet Page
+Here, users can link their Pi wallet to the platform. They just enter their wallet address and hit connect. After that, they'll see their wallet address and balance displayed. From there, they can jump straight into the game lobby or head to the currency exchange. They can also disconnect their wallet whenever they want.
